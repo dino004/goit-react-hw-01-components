4e3d52dcb5440211d30f6d194c11f1af17b40ed9
Dmytro Lytvyn <dmytro4lytvyn@gmail.com>
2023-06-13 08:28:22 +0300

    Приступив до другого завдання

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,19 @@
-import user from './user.json';
+import user from 'dataFiles/user.json';
+import data from 'dataFiles/data.json';
 import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
 
 export const App = () => {
   return (
-       <Profile
+    <>
+      <Profile
         username={user.username}
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics />
+    </>
   );
 };
