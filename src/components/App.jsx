801b59a99479fc17d284ab6ef01ebef9319ec9b2
Dmytro Lytvyn <dmytro4lytvyn@gmail.com>
2023-06-13 21:41:31 +0300

    Виконав четверте завдання

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import user from 'dataFiles/user.json';
 import data from 'dataFiles/data.json';
 import friends from 'dataFiles/friends.json';
+import transactions from 'dataFiles/transactions.json';
 
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
@@ -16,8 +18,9 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      <Statistics title="Upload stats" stats={data}/>
-      <FriendList friends={friends} />;
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
